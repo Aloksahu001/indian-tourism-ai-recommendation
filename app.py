5218dd867c2c002d271d5a16f153880cc02fdcd0
Alok Sahu <aloksahu72004@gmail.com>
2026-04-11 01:50:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -556,7 +556,7 @@
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host="0.0.0.0", port=port)
 
-```python
+
 from flask import Flask, render_template, request, redirect, session, url_for
 import pandas as pd
 import os
@@ -723,5 +723,4 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-```
 
